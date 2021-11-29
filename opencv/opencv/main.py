357622cfd1eb2559dccb2b22dedf9ef5d0503b74
권희재 <hjkwon93@fonetech.co.kr>
2021-11-29 10:20:59 +0900

    main 정리

--- opencv/opencv/main.py
+++ opencv/opencv/main.py
@@ -6,70 +6,15 @@ from pathlib import Path
 
 class ImageModifier:
     def __init__(self):
-<<<<<<< HEAD
-       # self.cat_dir = self.get_file_path("cat.jpg")
-       # self.image : np.ndarray = None
-=======
         self.gray_cat = self.get_file_path("gray_cat.jpg")
->>>>>>> c6e0566bbed54900e9e217f41cca091107647ac5
         return
 
     def get_file_path(self, file_name : str) -> str:
         pic_dir = str(Path(__file__).parent.resolve()) + str(Path("/pictures"))
         return pic_dir+str(Path(f"/{file_name}"))
 
-<<<<<<< HEAD
-
     def run(self):
-=======
-    def filter2D(self):
-        image = cv2.imread(self.gray_cat)
-        plt.imshow(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
-        plt.show()
-
-        size = 4
-        kernel = np.ones((size, size), np.float32) / (size ** 2)
-        print(kernel)
-
-        dst = cv2.filter2D(image, -1, kernel)
-        plt.imshow(cv2.cvtColor(dst, cv2.COLOR_BGR2RGB))
-        plt.show()
-        cv2.imwrite(self.get_file_path("blur_filter2D.jpg"), dst)
         return
-
-    def basic_blur(self):
-        image = cv2.imread(self.gray_cat)
-        plt.imshow(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
-        plt.show()
->>>>>>> c6e0566bbed54900e9e217f41cca091107647ac5
-
-        dst = cv2.blur(image, (4, 4))
-        plt.imshow(cv2.cvtColor(dst, cv2.COLOR_BGR2RGB))
-        plt.show()
-        cv2.imwrite(self.get_file_path("blur_basic.jpg"), dst)
-        return
-    
-    def Gaussian_blur(self):
-        image = cv2.imread(self.gray_cat)
-        plt.imshow(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
-        plt.show()
-
-        # kernel_size: 홀수
-        dst = cv2.GaussianBlur(image, (5, 5), 0)
-        plt.imshow(cv2.cvtColor(dst, cv2.COLOR_BGR2RGB))
-        plt.show()
-        cv2.imwrite(self.get_file_path("blur_Gaussian.jpg"), dst)
-        return
-
-
-
-
-    def run(self):
-        self.filter2D()
-        self.basic_blur()
-        self.Gaussian_blur()
-        return
-
 
 
 
