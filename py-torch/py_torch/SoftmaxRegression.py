c9c3bb3417d2019670285728cb0a7a9c875fe6ad
권희재 <hjkwon93@fonetech.co.kr>
2021-12-23 17:34:23 +0900

    Softmax MNIST 추가

--- py-torch/py_torch/SoftmaxRegression.py
+++ py-torch/py_torch/SoftmaxRegression.py
@@ -4,6 +4,9 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
+import torchvision.datasets as dsets
+import torchvision.transforms as transforms
+import random
 from torch.utils.data import TensorDataset  # 텐서데이터셋
 from torch.utils.data import DataLoader  # 데이터로더
 
@@ -172,11 +175,100 @@ class SoftmaxRegression:
 
         return
 
+    def __mnist_classification(self):
+        USE_CUDA = torch.cuda.is_available()  # GPU를 사용가능하면 True, 아니라면 False를 리턴
+        # GPU 사용 가능하면 사용하고 아니면 CPU 사용
+        device = torch.device("cuda" if USE_CUDA else "cpu")
+        print("다음 기기로 학습합니다:", device)
+
+        # for reproducibility
+        random.seed(777)
+        torch.manual_seed(777)
+        if device == 'cuda':
+            torch.cuda.manual_seed_all(777)
+
+        # hyperparameters
+        training_epochs = 15
+        batch_size = 100
+
+        # MNIST dataset
+        mnist_train = dsets.MNIST(
+            root='MNIST_data/',
+            train=True,
+            transform=transforms.ToTensor(),
+            download=True
+        )
+        mnist_test = dsets.MNIST(
+            root='MNIST_data/',
+            train=False,
+            transform=transforms.ToTensor(),
+            download=True
+        )
+
+        data_loader = DataLoader(
+            dataset=mnist_train, batch_size=batch_size, shuffle=True, drop_last=True)
+
+        # MNIST data image of shape 28 * 28 = 784
+        linear = nn.Linear(784, 10, bias=True).to(device=device)
+        # 비용 함수와 옵티마이저 정의
+        # 내부적으로 소프트맥스 함수를 포함하고 있음.
+        criterion = nn.CrossEntropyLoss().to(device)
+        optimizer = torch.optim.SGD(linear.parameters(), lr=0.1)
+        for epoch in range(training_epochs):  # 앞서 training_epochs의 값은 15로 지정함.
+            avg_cost = 0
+            total_batch = len(data_loader)
+
+            for X, Y in data_loader:
+                # 배치 크기가 100이므로 아래의 연산에서 X는 (100, 784)의 텐서가 된다.
+                X = X.view(-1, 28 * 28).to(device)
+                # 레이블은 원-핫 인코딩이 된 상태가 아니라 0 ~ 9의 정수.
+                Y = Y.to(device)
+
+                optimizer.zero_grad()
+                hypothesis = linear(X)
+                cost = criterion(hypothesis, Y)
+                cost.backward()
+                optimizer.step()
+
+                avg_cost += cost / total_batch
+
+            print('Epoch:', '%04d' % (epoch + 1),
+                  'cost =', '{:.9f}'.format(avg_cost))
+
+        print('Learning finished')
+
+        # 테스트 데이터를 사용하여 모델을 테스트한다.
+        with torch.no_grad():  # torch.no_grad()를 하면 gradient 계산을 수행하지 않는다.
+            X_test = mnist_test.test_data.view(-1, 28 * 28).float().to(device)
+            Y_test = mnist_test.test_labels.to(device)
+
+            prediction = linear(X_test)
+            correct_prediction = torch.argmax(prediction, 1) == Y_test
+            accuracy = correct_prediction.float().mean()
+            print('Accuracy:', accuracy.item())
+
+            # MNIST 테스트 데이터에서 무작위로 하나를 뽑아서 예측을 해본다
+            r = random.randint(0, len(mnist_test) - 1)
+            X_single_data = mnist_test.test_data[r:r +
+                                                 1].view(-1, 28 * 28).float().to(device)
+            Y_single_data = mnist_test.test_labels[r:r + 1].to(device)
+
+            print('Label: ', Y_single_data.item())
+            single_prediction = linear(X_single_data)
+            print('Prediction: ', torch.argmax(single_prediction, 1).item())
+
+            plt.imshow(
+                mnist_test.test_data[r:r + 1].view(28, 28), cmap='Greys', interpolation='nearest')
+            plt.show()
+
+        return
+
     def run(self):
         torch.manual_seed(1)
-        self.__cost_function()
-        self.__softmax_regression_low_level()
-        self.__softmax_regression_high_level()
-        self.__nnModule_softmax_regression()
+        # self.__cost_function()
+        # self.__softmax_regression_low_level()
+        # self.__softmax_regression_high_level()
+        # self.__nnModule_softmax_regression()
+        self.__mnist_classification()
 
         return
