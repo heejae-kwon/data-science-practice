44814682bfcdfd7bf5afc10274020f3958a3b86e
Heejae Kwon <sfhy33@gmail.com>
2022-01-12 07:17:18 +0900

    74p

--- Hands-On Machine Learning with Scikit-Learn, Keras, and TensorFlow/main.py
+++ Hands-On Machine Learning with Scikit-Learn, Keras, and TensorFlow/main.py
@@ -2,6 +2,29 @@ import os
 import tarfile
 import urllib.request
 import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+from zlib import crc32
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import StratifiedShuffleSplit
+
+
+def test_set_check(identifier, test_ratio):
+    return crc32(np.int64(identifier)) & 0xffffffff < test_ratio * 2**32
+
+
+def split_train_test_by_id(data, test_ratio, id_column):
+    ids = data[id_column]
+    in_test_set = ids.apply(lambda id_: test_set_check(id_, test_ratio))
+    return data.loc[~in_test_set], data.loc[in_test_set]
+
+
+def split_train_test(data, test_ratio):
+    shuffled_indices = np.random.permutation(len(data))
+    test_set_size = int(len(data) * test_ratio)
+    test_indices = shuffled_indices[:test_set_size]
+    train_indices = shuffled_indices[test_set_size:]
+    return data.iloc[train_indices], data.iloc[test_indices]
 
 
 def load_housing_data(housing_path):
@@ -25,8 +48,33 @@ def main():
 
     fetch_housing_data(housing_path=HOUSING_PATH, housing_url=HOUSING_URL)
 
-    data = load_housing_data(HOUSING_PATH)
-    print(data)
+    housing = load_housing_data(HOUSING_PATH)
+    print(housing.describe())
+    housing.hist(bins=50, figsize=(20, 15))
+    plt.show()
+
+    # 쌩으로
+    train_set, test_set = split_train_test(housing, 0.2)
+    # index
+    housing_with_id = housing.reset_index()   # adds an `index` column
+    train_set, test_set = split_train_test_by_id(housing_with_id, 0.2, "index")
+    # id with longtitude, latitude
+    housing_with_id["id"] = housing["longitude"] * 1000 + housing["latitude"]
+    train_set, test_set = split_train_test_by_id(housing_with_id, 0.2, "id")
+    # with sklearn
+    train_set, test_set = train_test_split(
+        housing, test_size=0.2, random_state=42)
+
+    housing["income_cat"] = pd.cut(housing["median_income"],
+                                   bins=[0., 1.5, 3.0, 4.5, 6., np.inf],
+                                   labels=[1, 2, 3, 4, 5])
+    housing["income_cat"].hist()
+    plt.show()
+
+    split = StratifiedShuffleSplit(n_splits=1, test_size=0.2, random_state=42)
+    for train_index, test_index in split.split(housing, housing["income_cat"]):
+        strat_train_set = housing.loc[train_index]
+        strat_test_set = housing.loc[test_index]
 
     return
 
