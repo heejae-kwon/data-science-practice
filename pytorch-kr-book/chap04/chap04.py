1d6f0452995841ca4e1c239965a13969701ae0ae
Heejae Kwon <sfhy33@gmail.com>
2022-08-18 15:10:55 +0900

    add chap04

---
+++ pytorch-kr-book/chap04/chap04.py
@@ -0,0 +1,9 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import seaborn as sns
+
+
+
+def run():
+    return
--- pytorch-kr-book/main.py
+++ pytorch-kr-book/main.py
@@ -1,10 +1,12 @@
 import chap02.chap02
 import chap03.chap03
+import chap04.chap04
 
 
 def main():
     # chap02.chap02.run()
-    chap03.chap03.run()
+    # chap03.chap03.run()
+    chap04.chap04.run()
 
     return
 
