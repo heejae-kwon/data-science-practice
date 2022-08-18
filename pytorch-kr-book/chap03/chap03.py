d542b1e5e21bcc046f79a774f63f0ea942f0ae04
Heejae Kwon <sfhy33@gmail.com>
2022-08-18 15:08:07 +0900

    chapter03 is done

--- pytorch-kr-book/chap03/chap03.py
+++ pytorch-kr-book/chap03/chap03.py
@@ -1,3 +1,4 @@
+from turtle import color
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
@@ -8,10 +9,12 @@ from pathlib import Path
 from sklearn import metrics, svm, datasets, model_selection, tree
 from sklearn.linear_model import LogisticRegression, LinearRegression
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler, MinMaxScaler
+from sklearn.preprocessing import StandardScaler, MinMaxScaler, normalize
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import accuracy_score, confusion_matrix
 from sklearn.cluster import KMeans
+from sklearn.cluster import DBSCAN
+from sklearn.decomposition import PCA
 
 
 def run_KNeighbor():
@@ -224,6 +227,85 @@ def run_KMean():
     return
 
 
+def run_PCA():
+    data_path = Path(str(Path.cwd())+'/chap03/data/credit_card.csv')
+    X = pd.read_csv(data_path)
+    X = X.drop('CUST_ID', axis=1)
+    X.fillna(method='ffill', inplace=True)
+    print(X.head())
+
+    scaler = StandardScaler()
+    X_scaled = scaler.fit_transform(X)
+
+    X_normalized = normalize(X_scaled)
+    X_normalized = pd.DataFrame(X_normalized)
+
+    pca = PCA(n_components=2)
+    X_principal = pca.fit_transform(X_normalized)
+    X_principal = pd.DataFrame(X_principal)
+    X_principal.columns = ['P1', 'P2']
+    print(X_principal.head())
+
+    db_default: DBSCAN = DBSCAN(eps=0.0375, min_samples=3).fit(X_principal)
+    labels = db_default.labels_
+
+    colours = {}
+    colours[0] = 'y'
+    colours[1] = 'g'
+    colours[2] = 'b'
+    colours[-1] = 'k'
+
+    cvec = [colours[label] for label in labels]
+
+    r = plt.scatter(X_principal['P1'], X_principal['P2'], color='y')
+    g = plt.scatter(X_principal['P1'], X_principal['P2'], color='g')
+    b = plt.scatter(X_principal['P1'], X_principal['P2'], color='b')
+    k = plt.scatter(X_principal['P1'], X_principal['P2'], color='k')
+
+    plt.figure(figsize=(9, 9))
+    plt.scatter(X_principal['P1'], X_principal['P2'], c=cvec)
+    plt.legend((r, g, b, k), ('Label 0', 'Label 1', 'Label 2', 'Label -1'))
+    plt.show()
+
+    # 모델 튜닝
+    db: DBSCAN = DBSCAN(eps=0.0375, min_samples=50).fit(X_principal)
+    labels1 = db.labels_
+
+    colours1 = {}
+    colours1[0] = 'y'
+    colours1[1] = 'g'
+    colours1[2] = 'b'
+    colours1[3] = 'c'
+    colours1[4] = 'y'
+    colours1[5] = 'm'
+    colours1[-1] = 'k'
+
+    cvec = [colours1[label] for label in labels1]
+
+    r = plt.scatter(X_principal['P1'], X_principal['P2'],
+                    marker='o', color=colours1[0])
+    g = plt.scatter(X_principal['P1'], X_principal['P2'],
+                    marker='o', color=colours1[1])
+    b = plt.scatter(X_principal['P1'], X_principal['P2'],
+                    marker='o', color=colours1[2])
+    c = plt.scatter(X_principal['P1'], X_principal['P2'],
+                    marker='o', color=colours1[3])
+    y = plt.scatter(X_principal['P1'], X_principal['P2'],
+                    marker='o', color=colours1[4])
+    m = plt.scatter(X_principal['P1'], X_principal['P2'],
+                    marker='o', color=colours1[5])
+    k = plt.scatter(X_principal['P1'], X_principal['P2'],
+                    marker='o', color=colours1[-1])
+
+    plt.figure(figsize=(9, 9))
+    plt.scatter(X_principal['P1'], X_principal['P2'], c=cvec)
+    plt.legend((r, g, b, c, y, m, k), ('Label 0', 'Label 1', 'Label 2',
+               'Label 3', 'Label 4', 'Label 5', 'Label -1'), scatterpoints=1, loc='upper left', ncol=3, fontsize=8)
+    plt.show()
+
+    return
+
+
 def run():
 
     # 지도학습
@@ -234,6 +316,7 @@ def run():
     # run_linearRegr()
 
     # 비지도학습
-    run_KMean()
+    # run_KMean()
+    run_PCA()
 
     return
