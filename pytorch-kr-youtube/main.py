d21a3604f8d29f6d3cb8316a6796bd55dcc808a2
Heejae Kwon <sfhy33@gmail.com>
2022-09-19 16:56:11 +0900

    logistic done

--- pytorch-kr-youtube/main.py
+++ pytorch-kr-youtube/main.py
@@ -1,11 +1,65 @@
 import numpy as np
 
 
+def simple_logistic_regression():
+    x_data = np.array([2, 4, 6, 8, 10, 12, 14, 16, 18, 20]).reshape(10, 1)
+    t_data = np.array([0, 0, 0, 0, 0, 0, 1, 1, 1, 1]).reshape(10, 1)
+
+    W = np.random.rand(1, 1)  # (input, 1)
+    b = np.random.rand(1)
+    print(f'W={W} W.shape={W.shape} b={b} b.shape={b.shape}')
+
+    def sigmoid(x):
+        return 1 / (1+np.exp(-x))
+
+    def loss_func(x: np.ndarray, t: np.ndarray):
+        delta = 1e-7
+
+        z = np.dot(x, W)+b
+        y = sigmoid(z)
+        # cross-entropy
+        return -np.sum(t*np.log(y+delta) + (1-t)*np.log((1-y)+delta))
+
+    def error_val(x: np.ndarray, t: np.ndarray):
+        delta = 1e-7
+
+        z = np.dot(x, W)+b
+        y = sigmoid(z)
+        # cross-entropy
+        return -np.sum(t*np.log(y+delta) + (1-t)*np.log((1-y)+delta))
+
+    def predict(x):
+        z = np.dot(x, W)+b
+        y = sigmoid(z)
+
+        if y > 0.5:
+            result = 1
+        else:
+            result = 0
+
+        return y, result
+
+    learning_rate = 1e-2
+    def f(x): return loss_func(x_data, t_data)
+    print(
+        f'Initial error value={error_val(x_data,t_data)} Initial W={W}, b={b}')
+
+    for step in range(10001):
+        W -= learning_rate * numerical_derivative(f, W)
+        b -= learning_rate*numerical_derivative(f, b)
+        if step % 400 == 0:
+            print(
+                f'step={step}, error_value={error_val(x_data,t_data)}, W={W}, b={b}')
+
+    (real_val, logical_val) = predict(17)
+    print(real_val, logical_val)
+
+
 def simple_regression():
     x_data = np.array([1, 2, 3, 4, 5]).reshape(5, 1)
     t_data = np.array([2, 3, 4, 5, 6]).reshape(5, 1)
 
-    W = np.random.rand(1, 1) #(input, 1)
+    W = np.random.rand(1, 1)  # (input, 1)
     b = np.random.rand(1)
     print(f'W={W} W.shape={W.shape} b={b} b.shape={b.shape}')
 
@@ -32,7 +86,7 @@ def simple_regression():
         if step % 400 == 0:
             print(
                 f'step={step}, error_value={error_val(x_data,t_data)}, W={W}, b={b}')
-    
+
     print(predict(43))
 
     return
@@ -60,7 +114,8 @@ def numerical_derivative(f, x: np.ndarray):
 
 
 def main():
-    simple_regression()
+    # simple_regression()
+    simple_logistic_regression()
     return
 
 
