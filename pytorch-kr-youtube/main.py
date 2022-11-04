dab1023834a1881adb2607e345b26f0ad7dd6457
Heejae Kwon <sfhy33@gmail.com>
2022-11-04 15:19:12 +0900

    deep learning done

--- pytorch-kr-youtube/main.py
+++ pytorch-kr-youtube/main.py
@@ -3,6 +3,10 @@ from torchvision import datasets, transforms
 import torch
 
 
+def sigmoid(x):
+    return 1 / (1+np.exp(-x))
+
+
 def simple_logistic_regression():
     x_data = np.array([2, 4, 6, 8, 10, 12, 14, 16, 18, 20]).reshape(10, 1)
     t_data = np.array([0, 0, 0, 0, 0, 0, 1, 1, 1, 1]).reshape(10, 1)
@@ -10,9 +14,6 @@ def simple_logistic_regression():
     W = np.random.rand(1, 1)  # (input, 1)
     b = np.random.rand(1)
     print(f'W={W} W.shape={W.shape} b={b} b.shape={b.shape}')
-
-    def sigmoid(x):
-        return 1 / (1+np.exp(-x))
 
     def loss_func(x: np.ndarray, t: np.ndarray):
         delta = 1e-7
@@ -184,28 +185,107 @@ class LogicGate:
         return y, result
 
 
-def run_mnist():
-
-    train_data: torch.Tensor = (datasets.MNIST(root='./data',
-                                               train=True,
-                                               download=True,
-                                               transform=transforms.ToTensor()))
-    test_data: torch.Tensor = (datasets.MNIST(root='./data',
-                                              train=False,
-                                              download=True,
-                                              transform=transforms.ToTensor()))
-
-    print(
-        f"trainig_data.shape = {train_data.shape}, test_data.shape = {test_data.shape}")
-
-    return
-
-
 class NeuralNetwork:
     def __init__(self, input_nodes, hidden_nodes, output_nodes) -> None:
+        self.target_data = np.zeros()
+        self.input_data = np.zeros()
+
         self.input_nodes = input_nodes
         self.hidden_nodes = hidden_nodes
         self.output_nodes = output_nodes
+
+        self.W2 = np.random.rand(self.input_nodes, self.hidden_nodes)
+        self.b2 = np.random.rand(self.hidden_nodes)
+
+        self.W3 = np.random.rand(self.hidden_nodes, self.output_nodes)
+        self.b3 = np.random.rand(self.output_nodes)
+
+        self.learning_rate = 1e-4
+
+    def feed_forward(self):
+        delta = 1e-7
+
+        z1 = np.dot(self.input_data, self.W2) + self.b2
+        y1 = sigmoid(z1)
+
+        z2 = np.dot(y1, self.W3) + self.b3
+        y = sigmoid(z2)
+
+        # cross_entropy
+        return -np.sum(self.target_data + np.log(y+delta) + (1-self.target_data) + np.log((1-y)+delta))
+
+    def loss_val(self):
+        return self.feed_forward()
+
+    def train(self, training_data):
+        self.target_data = np.zeros(self.output_nodes) + 0.01
+        self.target_data[int(training_data[0])] = 0.99
+        self.input_data = (training_data[1:]/255.0*0.99)+0.01
+
+        def f(x): return self.feed_forward()
+        print("Initial loss value = ", self.loss_val())
+
+        for step in range(10001):
+            self.W2 -= self.learning_rate * numerical_derivative(f, self.W2)
+            self.b2 -= self.learning_rate * numerical_derivative(f, self.b2)
+            self.W3 -= self.learning_rate * numerical_derivative(f, self.W3)
+            self.b3 -= self.learning_rate * numerical_derivative(f, self.b3)
+
+    def predict(self, input_data):
+        z1 = np.dot(input_data, self.W2) + self.b2
+        y1 = sigmoid(z1)
+
+        z2 = np.dot(y1, self.W3) + self.b3
+        y = sigmoid(z2)
+
+        return np.argmax(y)
+
+    def accuracy(self, test_data):
+        matched_list = []
+        not_matched_list = []
+
+        for index in range(len(test_data)):
+            label = int(test_data[index, 0])  # 정답
+
+            # normalize
+            data = (test_data[index, 1:]/255.0*0.99) + 0.01
+            predicted_num = self.predict(data)
+
+            if label == predicted_num:
+                matched_list.append(index)
+            else:
+                not_matched_list.append(index)
+
+        print(f"Current Accuracy = {100*(len(matched_list)/(len(test_data))) %}")
+
+        return matched_list, not_matched_list
+
+
+def run_mnist():
+    traning_data = np.loadtxt(
+        './data/MNIST/mnist_train.csv', delimiter=',', dtype=np.float32)
+
+    test_data = np.loadtxt(
+        './data/MNIST/mnist_test.csv', delimiter=',', dtype=np.float32)
+
+    print(
+        f'traing_data.shape = {traning_data.shape}, test_data.shape = {test_data.shape}')
+
+    input_nodes = 784
+    hidden_nodes = 100
+    output_nodes = 10
+
+    nn = NeuralNetwork(input_nodes, hidden_nodes, output_nodes)
+
+    for step in range(30001):
+        index = np.random.randint(0, len(traning_data)-1)
+        nn.train(traning_data[index])
+        if step % 400 == 0:
+            print(f"step = {step}, loss_val = {nn.loss_val()}")
+
+    nn.accuracy(test_data)
+
+    return
 
 
 def main():
