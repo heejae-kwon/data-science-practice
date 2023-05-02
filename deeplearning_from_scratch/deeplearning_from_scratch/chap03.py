4954336f5cc5e862d07e22dcda52de7746809c59
Heejae Kwon <sfhy33@gmail.com>
2023-05-02 12:41:58 +0900

    chap03 init_network

---
+++ deeplearning_from_scratch/deeplearning_from_scratch/chap03.py
@@ -0,0 +1,82 @@
+import numpy as np
+import matplotlib.pylab as plt
+
+# 계단함수
+
+
+def step_function(x: np.ndarray):
+    return np.array(x > 0, dtype=np.int64)
+
+
+def sigmoid(x: np.ndarray):
+    return 1 / (1+np.exp(-x))
+
+
+def relu(x: np.ndarray):
+    return np.maximum(0, x)
+
+
+def identity_funtion(x):
+    return x
+
+
+def init_network():
+    network = {}
+    network['W1'] = np.array([[0.1, 0.3, 0.5], [0.2, 0.4, 0.6]])
+    network['b1'] = np.array([0.1, 0.2, 0.3])
+    network['W2'] = np.array([[0.1, 0.4], [0.2, 0.5], [0.3, 0.6]])
+    network['b2'] = np.array([0.1, 0.2])
+    network['W3'] = np.array([[0.1, 0.3], [0.2, 0.4]])
+    network['b3'] = np.array([0.1, 0.2])
+
+    return network
+
+
+x = np.arange(-5, 5, 0.1)
+y = step_function(x)
+plt.plot(x, y)
+plt.ylim(-0.1, 1.1)
+plt.show()
+
+x = np.arange(-5, 5, 0.1)
+y = sigmoid(x)
+plt.plot(x, y)
+plt.ylim(-0.1, 1.1)
+plt.show()
+
+X = np.array([1, 2])
+print(X.shape)
+W = np.array([[1, 3, 5], [2, 4, 6]])
+print(W)
+print(W.shape)
+Y = np.dot(X, W)
+print(Y)
+
+X = np.array([1.0, 0.5])
+W1 = np.array([[0.1, 0.3, 0.5], [0.2, 0.4, 0.6]])
+B1 = np.array([0.1, 0.2, 0.3])
+
+print(W1.shape)
+print(X.shape)
+print(B1.shape)
+
+A1 = np.dot(X, W1) + B1
+Z1 = sigmoid(A1)
+print(A1)
+print(Z1)
+
+W2 = np.array([[0.1, 0.4], [0.2, 0.5], [0.3, 0.6]])
+B2 = np.array([0.1, 0.2])
+
+print(Z1.shape)
+print(W2.shape)
+print(B2.shape)
+
+A2 = np.dot(Z1, W2)
+Z2 = sigmoid(A2)
+
+W3 = np.array([[0.1, 0.3], [0.2, 0.4]])
+B3 = np.array([0.1, 0.2])
+
+A3 = np.dot(Z2, W3)+B3
+Y = identity_funtion(A3)
