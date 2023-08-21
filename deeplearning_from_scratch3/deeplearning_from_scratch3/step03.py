86c5c6beba1a7b938abb80f212a0f9f7d74b3a21
Heejae Kwon <sfhy33@gmail.com>
2023-08-21 10:45:16 +0900

    step06 done

--- deeplearning_from_scratch3/deeplearning_from_scratch3/step03.py
+++ deeplearning_from_scratch3/deeplearning_from_scratch3/step03.py
@@ -2,12 +2,11 @@ import numpy as np
 from step01 import Variable
 from step02 import Function, Square
 
-
 class Exp(Function):
     def forward(self, x):
         return np.exp(x)
 
-
+#됬거든요 어이어없
 if __name__ == "__main__":
     A = Square()
     B = Exp()
